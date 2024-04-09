c691c70069dacd581799728513ba6c88e956e2ba
Virham <71260381+Virham@users.noreply.github.com>
2024-04-09 10:35:37 +0200

    startet casablanca tull

--- Bot/cppversjon/restAPI.hpp
+++ Bot/cppversjon/restAPI.hpp
@@ -1,11 +1,5 @@
-#ifndef restAPI_HPP
-#define restAPI_HPP
-
-#include <iostream>
-#include "pistache/endpoint.h"
-#include "gameLogic.hpp"
 
 
-using namespace Pistache
 
-
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
